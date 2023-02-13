fb9da86d9b747167da264260081a2e440937e722
Xagaa <102919428+Xagaa@users.noreply.github.com>
2023-02-14 00:34:34 +0700

    Add files via upload

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/spark_munch.mk
+
+COMMON_LUNCH_CHOICES := \
+    spark_munch-user \
+    spark_munch-userdebug \
+    spark_munch-eng
